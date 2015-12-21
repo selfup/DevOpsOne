2095a43339a9655619260470cd97a8fe86522ec8
Regis Boudinot <selfup@users.noreply.github.com>
2015-12-20 20:37:53 -0700

    Update create_db_yml_server.rb

--- create_db_yml_server.rb
+++ create_db_yml_server.rb
@@ -13,32 +13,32 @@ class CreateDB
 
 	def new_yml
   "default: &default\n
-	adapter: postgresql
-	encoding: unicode
-	pool: 5\n
-	host: localhost
-	username: #{find_app_name}
-	password: <%= ENV['#{find_app_name.upcase}_DATABASE_PASSWORD'] %>\n
+  adapter: postgresql
+  encoding: unicode
+  pool: 5\n
+  host: localhost
+  username: #{find_app_name}
+  password: <%= ENV['#{find_app_name.upcase}_DATABASE_PASSWORD'] %>\n
 development:
-	<<: *default
-	database: #{find_app_name}_development
-	username: #{find_app_name}
-	password: <%= ENV['#{find_app_name.upcase}_DATABASE_PASSWORD'] %>\n
+  <<: *default
+  database: #{find_app_name}_development
+  username: #{find_app_name}
+  password: <%= ENV['#{find_app_name.upcase}_DATABASE_PASSWORD'] %>\n
 test:
-	<<: *default
-	database: #{find_app_name}_test
-	username: #{find_app_name}
-	password: <%= ENV['#{find_app_name.upcase}_DATABASE_PASSWORD'] %>\n
+  <<: *default
+  database: #{find_app_name}_test
+  username: #{find_app_name}
+  password: <%= ENV['#{find_app_name.upcase}_DATABASE_PASSWORD'] %>\n
 production:
-	<<: *default
-	database: #{find_app_name}_production
-	username: #{find_app_name}
-	password: <%= ENV['#{find_app_name.upcase}_DATABASE_PASSWORD'] %>"
+  <<: *default
+  database: #{find_app_name}_production
+  username: #{find_app_name}
+  password: <%= ENV['#{find_app_name.upcase}_DATABASE_PASSWORD'] %>"
 	end
 
 	def rbenv_vars
-		"#{find_app_name.upcase}_DATABASE_USERNAME=#{find_app_name}"
-		"#{find_app_name.upcase}_DATABASE_PASSWORD=#{ARGV[0]}"
+	  "#{find_app_name.upcase}_DATABASE_USERNAME=#{find_app_name}"
+	  "#{find_app_name.upcase}_DATABASE_PASSWORD=#{ARGV[0]}"
 	end
 
 	def replace_file

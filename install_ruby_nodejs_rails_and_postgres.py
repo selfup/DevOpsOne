f367d2c6e1a9a7d6f4188e2dad6d135fdcbd494e
selfup <boudinot.regis@yahoo.com>
2015-12-22 17:47:20 -0700

    more structured way to install everything

---
+++ install_ruby_nodejs_rails_and_postgres.py
@@ -0,0 +1,8 @@
+import os
+os.system('rbenv install 2.2.3')
+os.system('rbenv global 2.2.3')
+os.system('ruby -v')
+os.system('dpkg --get-selections | grep node && sudo apt-get purge nodejs npm && curl -sL https://deb.nodesource.com/setup | sudo bash - && sudo apt-get install -y nodejs')
+os.system('sudo npm cache clean -f && sudo npm install -g n && sudo n stable')
+os.system('gem install rails -v 4.2.4 --no-rdoc --no-ri && rbenv rehash && gem install pry && gem install ag')
+os.system('mkdir -p ~/.rbenv/plugins && cd ~/.rbenv/plugins && git clone https://github.com/sstephenson/rbenv-vars.git && cd && sudo apt-get install postgresql postgresql-contrib libpq-dev -y')

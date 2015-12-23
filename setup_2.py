d52aa2aa0a2cea064d5fe94e0df923391e9c3b22
selfup <boudinot.regis@yahoo.com>
2015-12-22 19:37:12 -0700

    new idea

---
+++ setup_2.py
@@ -0,0 +1,6 @@
+os.system('echo export PATH="$HOME/.rbenv/bin:$PATH" >> ~/.bashrc')
+os.system('echo export PATH="$HOME/.rbenv/bin:$PATH" >> ~/.bash_profile')
+os.system('echo eval "$(rbenv init -)" >> ~/.bashrc')
+os.system('echo eval "$(rbenv init -)" >> ~/.bash_profile')
+os.system('git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build')
+os.system('exec $SHELL')

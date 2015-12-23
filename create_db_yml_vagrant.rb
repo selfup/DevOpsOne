28c3e932f109714fe44fb635dfc072b416e9cb37
selfup <boudinot.regis@yahoo.com>
2015-12-22 19:02:43 -0700

    added uncode spec for vagrant hashicorp32

---
+++ create_db_yml_vagrant.rb
@@ -0,0 +1,66 @@
+require 'pry'
+
+class CreateDB
+  def target_directory
+    `pwd`
+  end
+
+  def find_app_name
+    t_dir = target_directory
+    app_name = t_dir.split("/")
+    dbyml_app_name = app_name.last.gsub("\n", "")
+  end
+
+  def new_yml
+  "default: &default\n
+  adapter: postgresql
+  encoding: LATIN1
+  pool: 5\n
+  host: localhost
+  username: #{find_app_name}
+  password: <%= ENV['#{find_app_name.upcase}_DATABASE_PASSWORD'] %>\n
+development:
+  <<: *default
+  database: #{find_app_name}_development
+  username: #{find_app_name}
+  password: <%= ENV['#{find_app_name.upcase}_DATABASE_PASSWORD'] %>\n
+test:
+  <<: *default
+  database: #{find_app_name}_test
+  username: #{find_app_name}
+  password: <%= ENV['#{find_app_name.upcase}_DATABASE_PASSWORD'] %>\n
+production:
+  <<: *default
+  database: #{find_app_name}_production
+  username: #{find_app_name}
+  password: <%= ENV['#{find_app_name.upcase}_DATABASE_PASSWORD'] %>"
+  end
+
+  def rbenv_vars
+    "\n\n"
+    "#{find_app_name.upcase}_DATABASE_USERNAME=#{find_app_name}"
+    "#{find_app_name.upcase}_DATABASE_PASSWORD=#{ARGV[0]}"
+  end
+
+  def replace_file
+    `cd #{target_directory}`
+    `rm -rf config/database.yml`
+    `cd #{target_directory}`
+    `echo "#{new_yml}" >> config/database.yml`
+    `cd #{target_directory}`
+    `echo "#{rbenv_vars}" >> .rbenv-vars`
+  end
+
+  def create_pg_user
+    `sudo -u postgres createuser -s "#{find_app_name}"`
+  end
+
+  def do_it_all
+    replace_file
+    create_pg_user
+    puts "#{find_app_name}"
+  end
+end
+
+h = CreateDB.new
+h.do_it_all

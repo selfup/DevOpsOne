e5a6835d211dcb543efc02b56288792eb2875272
selfup <boudinot.regis@yahoo.com>
2015-12-27 00:21:22 -0700

    pulled down new README change - removed require 'pry' - new logic for ensure.rb

--- create_db_yml_OSX.rb
+++ create_db_yml_OSX.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class CreateDB
 
   def initialize
--- ensure.rb
+++ ensure.rb
@@ -1,5 +1,3 @@
-`python ~/DevOpsOne/setup_1.py`
-`python ~/DevOpsOne/setup_2.py`
-`python ~/DevOpsOne/setup_3.py`
+`python ~/DevOpsOne/setup_1.py && python ~/DevOpsOne/setup_2.py && python ~/DevOpsOne/setup_3.py`
 `rbenv global 2.2.3`
 `rbenv rehash`

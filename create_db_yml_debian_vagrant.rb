5dd67604b50169c616a81847dd6263f41677be61
selfup <boudinot.regis@yahoo.com>
2015-12-29 20:21:59 -0700

    for debian vagrant box

---
+++ create_db_yml_debian_vagrant.rb
@@ -0,0 +1,78 @@
+class CreateDB
+
+  def initialize
+    @sec  ||= secret
+    @vars ||= rbenv_vars
+  end
+
+  def target_directory
+    `pwd`
+  end
+
+  def find_app_name
+    t_dir = target_directory
+    app_name = t_dir.split("/")
+    dbyml_app_name = app_name.last.gsub("\n", "")
+    dbyml_app_name = dbyml_app_name.gsub("-", "")
+  end
+
+  def new_yml
+  "default: &default\n
+  adapter: postgresql
+  encoding: unicode
+  pool: 5\n
+  host: localhost
+  username: #{find_app_name}
+  password: <%= ENV['#{find_app_name.upcase}_DATABASE_PASSWORD'] %>\n
+development:
+  <<: *default
+  database: #{find_app_name}_development
+  username: #{find_app_name}
+  password: <%= ENV['#{find_app_name.upcase}_DATABASE_PASSWORD'] %>\n
+test:
+  <<: *default
+  database: #{find_app_name}_test
+  username: #{find_app_name}
+  password: <%= ENV['#{find_app_name.upcase}_DATABASE_PASSWORD'] %>\n
+production:
+  <<: *default
+  database: #{find_app_name}_production
+  username: #{find_app_name}
+  password: <%= ENV['#{find_app_name.upcase}_DATABASE_PASSWORD'] %>"
+  end
+
+  def secret
+    `rake secret`
+  end
+
+  def rbenv_vars
+    "SECRET_KEY_BASE=#{@sec}
+#{find_app_name.upcase}_DATABASE_USERNAME=#{find_app_name}
+#{find_app_name.upcase}_DATABASE_PASSWORD=#{ARGV[0]}"
+  end
+
+  def replace_file
+    `cd #{target_directory}`
+    `rm -rf config/database.yml`
+    `cd #{target_directory}`
+    `echo "#{new_yml}" >> config/database.yml`
+    `cd #{target_directory}`
+    `echo "#{@vars}" >> .rbenv-vars`
+    `cd #{target_directory}`
+    `echo "*/application.yml" >> .gitignore`
+    `echo ".rbenv-vars" >> .gitignore`
+  end
+
+  def create_pg_user
+    `sudo -u postgres createuser -s "#{find_app_name}"`
+  end
+
+  def do_it_all
+    replace_file
+    create_pg_user
+    puts "#{find_app_name}"
+  end
+end
+
+h = CreateDB.new
+h.do_it_all

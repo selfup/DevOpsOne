166fde2e3fdcf9b939f7cbebe84a75b7d9ac9116
Regis <boudinot.regis@yahoo.com>
2016-01-26 10:13:04 -0700

    Now ruby 2.3.0 and rails 4.2.5 since they are stable - looking forward to rails 5

--- install_rest.py
+++ install_rest.py
@@ -1,5 +1,5 @@
 import os
 os.system('curl -sL https://deb.nodesource.com/setup | sudo bash - && sudo apt-get install -y nodejs')
 os.system('sudo npm cache clean -f && sudo npm install -g n && sudo n stable')
-os.system('gem install rails -v 4.2.4 --no-rdoc --no-ri && rbenv rehash && gem install pry && gem install ag')
+os.system('gem install rails -v 4.2.5 --no-rdoc --no-ri && rbenv rehash && gem install pry && gem install ag')
 os.system('mkdir -p ~/.rbenv/plugins && cd ~/.rbenv/plugins && git clone https://github.com/sstephenson/rbenv-vars.git && cd && sudo apt-get install postgresql postgresql-contrib libpq-dev -y')
--- install_ruby.py
+++ install_ruby.py
@@ -1,4 +1,4 @@
 import os
-os.system('rbenv install 2.2.3')
-os.system('rbenv global 2.2.3')
+os.system('rbenv install 2.3.0')
+os.system('rbenv global 2.3.0')
 os.system('rbenv rehash')

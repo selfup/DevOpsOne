6b4724927d6bc4148ae3255417c592ce41a81c4d
Regis <boudinot.regis@yahoo.com>
2016-02-17 13:36:45 -0700

    going to test with Ubuntu 14.04 Vagrant Machine

--- setup_3.py
+++ setup_3.py
@@ -2,4 +2,3 @@ import os
 os.system('echo export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH" >> ~/.bashrc')
 os.system('echo export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH" >> ~/.bash_profile')
 os.system('git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash')
-os.system('exec $SHELL')

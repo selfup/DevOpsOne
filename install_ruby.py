7b83fdfe17f170ad67d299e9215b3538ac72c9ab
Regis <boudinot.regis@yahoo.com>
2016-02-17 14:12:20 -0700

    path set up right

--- install_ruby.py
+++ install_ruby.py
@@ -2,3 +2,5 @@ import os
 os.system('rbenv install 2.3.0')
 os.system('rbenv global 2.3.0')
 os.system('rbenv rehash')
+os.system('export PATH="$HOME/.rbenv/bin:$PATH" && eval "$(rbenv init -)"')
+os.system('ruby -v')

82602c886d3c161149280175eeb2e8e4d8322ae0
Regis <boudinot.regis@yahoo.com>
2017-07-04 14:27:04 -0500

    testing things out

--- install_ruby.py
+++
@@ -1,4 +0,0 @@
-import os
-os.system('rbenv install 2.3.0')
-os.system('rbenv global 2.3.0')
-os.system('rbenv rehash')
--- setup_2.py
+++
@@ -1,7 +0,0 @@
-import os
-os.system('echo export PATH="$HOME/.rbenv/bin:$PATH" >> ~/.bashrc')
-os.system('echo export PATH="$HOME/.rbenv/bin:$PATH" >> ~/.bash_profile')
-os.system('echo eval "$(rbenv init -)" >> ~/.bashrc')
-os.system('echo eval "$(rbenv init -)" >> ~/.bash_profile')
-os.system('git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build')
-os.system('exec $SHELL')
